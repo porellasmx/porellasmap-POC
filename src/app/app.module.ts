32535ebf7aecd360f6e1bc448e64e29b42b92d63
Patricio <wearetamo@gmail.com>
2019-08-22 00:48:37 -0400

    store page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { DashboardPageComponent } from './dashboard-page/dashboard-page.component';
+import { StorePageComponent } from './store-page/store-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomePageComponent,
-    DashboardPageComponent
+    DashboardPageComponent,
+    StorePageComponent
   ],
   imports: [
     BrowserModule,
