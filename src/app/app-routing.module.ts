67853bbf08fcb7cb64e84ab4d09ed95d10f02fec
Patricio <wearetamo@gmail.com>
2019-08-22 14:45:31 -0400

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardPageComponent } from './dashboard-page/dashboard-page.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '*',
+    component: HomePageComponent
+  },
+  {
+    path: 'dashboard',
+    component: DashboardPageComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
