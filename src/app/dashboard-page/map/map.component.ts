cc99bf93a130682175000bd13b25aa12f7e3f831
Patricio Vargas <pevargas2730@gmail.com>
2019-08-31 04:51:11 -0400

    map basic variables

--- src/app/dashboard-page/map/map.component.ts
+++ src/app/dashboard-page/map/map.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-
+import { darkStyle } from './map-styles';
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.scss']
 })
 export class MapComponent implements OnInit {
+  lat = 51.678418;
+  lng = 7.809007;
+  style = darkStyle;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
