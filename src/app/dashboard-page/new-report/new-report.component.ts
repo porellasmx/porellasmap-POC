1a03b9b059f50505e77aee35e8e51d60a6261d11
Patricio Vargas <pevargas2730@gmail.com>
2019-08-30 23:49:00 -0400

    styling form

--- src/app/dashboard-page/new-report/new-report.component.ts
+++ src/app/dashboard-page/new-report/new-report.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { GooglePlaceDirective } from 'ngx-google-places-autocomplete';
+import { Address } from 'ngx-google-places-autocomplete/objects/address';
 
 @Component({
   selector: 'app-new-report',
@@ -6,10 +8,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./new-report.component.scss']
 })
 export class NewReportComponent implements OnInit {
-
-  constructor() { }
+  @ViewChild('placesRef', { static: false }) placesRef: GooglePlaceDirective;
+  abuseList: string[];
+  options = {
+    types: ['address'],
+    componentRestrictions: { country: 'mx' }
+  };
+  constructor() {}
 
   ngOnInit() {
+    this.buildAbuseList();
   }
 
+  handleAddressChange(address: Address) {
+    console.log(address.geometry.location.lng());
+  }
+
+  buildAbuseList(): void {
+    this.abuseList = ['Sexual', 'Verbal', 'Psicologico', 'Fisico'];
+  }
 }
