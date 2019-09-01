f5d476c7431353950a5eb6422f50ecfa49a06e53
Patricio Vargas <pevargas2730@gmail.com>
2019-09-01 00:26:13 -0400

    marker methods added

--- src/app/shared/components/map/map.component.ts
+++ src/app/shared/components/map/map.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { darkStyle } from './map-styles';
+import { Observable } from 'rxjs';
+import { Marker } from '../../models/marker.model';
+import { MapService } from '../../services/map.service';
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
@@ -9,7 +12,38 @@ export class MapComponent implements OnInit {
   lat = 19.432608;
   lng = -99.133209;
   style = darkStyle;
-  constructor() {}
+  $newMarker: Observable<Marker>;
 
-  ngOnInit() {}
+  constructor(private mapService: MapService) {}
+
+  ngOnInit() {
+    this.getNewMarker();
+  }
+
+  getNewMarker(): void {
+    this.$newMarker = this.mapService.$tempMarker;
+  }
+
+  markerDragEnd(marker: Marker, $event: MouseEvent) {
+    console.log('dragEnd', marker, $event);
+  }
+
+  markerOver(marker: Marker) {
+    marker.animation = 'BOUNCE';
+  }
+
+  markerOut(marker: Marker) {
+    marker.animation = '';
+  }
+
+  mapClicked($event: any) {
+    const newMarker = {
+      lat: $event.coords.lat,
+      long: $event.coords.lng,
+      draggable: true,
+      animation: 'DROP'
+    };
+
+    this.mapService.setTempMarker(newMarker);
+  }
 }
