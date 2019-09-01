9c1d9ff7e550a243233c71588b000441f86533b5
Patricio Vargas <pevargas2730@gmail.com>
2019-08-31 20:04:46 -0400

    report interface

---
+++ src/app/shared/models/report.model.ts
@@ -0,0 +1,11 @@
+import { Marker } from './marker.model';
+
+export interface Report {
+  address: string;
+  placeName: string;
+  description: string;
+  abuseType: string;
+  dateOfEvent: Date;
+  marker: Marker;
+  imageName: any;
+}
