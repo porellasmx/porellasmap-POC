d732768835e7aebe6377b690f5b073ebfde9b7b3
Patricio Vargas <pevargas2730@gmail.com>
2019-09-04 03:23:38 -0400

    snackbar show/hide

--- src/app/shared/services/ui.service.ts
+++ src/app/shared/services/ui.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
+import { MatSnackBar } from '@angular/material';
 
 @Injectable({
   providedIn: 'root'
@@ -7,9 +8,16 @@ import { BehaviorSubject } from 'rxjs';
 export class UiService {
   private toggleSidenavSource = new BehaviorSubject(false);
   $toggleSidenav = this.toggleSidenavSource.asObservable();
-  constructor() {}
+
+  constructor(private snackBar: MatSnackBar) {}
 
   setToggleSidenav(flag: boolean): void {
     this.toggleSidenavSource.next(flag);
+  }
+
+  setSnackBar(message: string, time: number): void {
+    this.snackBar.open(message, 'Undo', {
+      duration: time
+    });
   }
 }
